ed20f6466d1d62953d0f27b4edacb1da0c1f7648
Saumyadeep Mishra <sdm.cord7@gmail.com>
2026-07-22 15:40:36 +0530

    bug: fourth box flicker

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function levelUp(){
     level++;
     h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randColor = box[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
     game.push(randColor);
